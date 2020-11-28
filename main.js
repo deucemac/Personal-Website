e20f1fd0f4a34d7253a807083d90a675522661cb
Dwayne McDowell <dwaynemcdowell@Dwaynes-MacBook-Pro.local>
2020-11-28 17:46:41 -0500

    fixed header

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 
+
 new fullpage('#fullpage', {
+  sectionsColor: ['#B8AE9C', '#348899', '#F2AE72', '#5C832F', '#B8B89F'],
   autoScrolling: true,
   navigation: true,
   navigationPosition: 'right',
   navigationTooltips: ['First page', 'Second page', 'Third page', 'Last page']
 });
+
