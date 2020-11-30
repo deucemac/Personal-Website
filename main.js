748e7c4e489b137fc75a1033aa10a2d7dee7d092
Dwayne McDowell <dwaynemcdowell@Dwaynes-MacBook-Pro.local>
2020-11-29 22:43:22 -0500

    animated line

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
+let nameIntro = document.querySelector('.intro')
+// nameIntro.style.textDecoration = "none"
+let line = document.querySelector('.line')
+window.onload = function () {
+  setTimeout(function () {
+    line.style.transform = "scaleX(1)"
+    line.style.visibility = "visible"
+  }, 1000)
+}
 
 
 new fullpage('#fullpage', {
-  sectionsColor: ['#B8AE9C', '#348899', '#F2AE72', '#5C832F', '#B8B89F'],
+  sectionsColor: ['#000000', '#348899', '#F2AE72', '#5C832F', '#B8B89F'],
   autoScrolling: true,
   navigation: true,
   navigationPosition: 'right',
-  navigationTooltips: ['First page', 'Second page', 'Third page', 'Last page']
+  navigationTooltips: ['First page', 'Second page', 'Third page', 'Last page'],
+  anchors: ['firstPage', 'secondPage', 'thirdPage', 'lastPage'], 
+  menu: '#myMenu'
 });
 
